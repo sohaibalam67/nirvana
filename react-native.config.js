89aceb7d6e44a05b30bb0ce49337a000c3b67238
sohaibalam67 <sohaibalam67@gmail.com>
2020-03-27 23:26:19 +0530

    Update Android and iOS folders
    - while upgrading to react-native 0.62 the android and iOS folders were
    not updated.

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: [
+    './src/assets/fonts/',
+    './src/assets/images/',
+    'react-native-vector-icons',
+  ],
+};

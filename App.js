90d474daa24a0428ffb6211ae60fac8ea6a66021
sohaibalam67 <sohaibalam67@gmail.com>
2020-03-28 21:41:43 +0530

    Add `redux-persist` and use `asyncstorage` as the storage engine

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {Provider} from 'react-redux';
 import React, {Component} from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
+import {PersistGate} from 'redux-persist/integration/react';
 import colors from './src/values/colors';
 import RootScreen from './src/screens/RootScreen';
 import configureStore from './src/store/configureStore';
@@ -9,13 +10,15 @@ const store = configureStore();
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={store.store}>
         <SafeAreaView style={{flex: 1, backgroundColor: colors.JET_BLACK}}>
           <StatusBar
             backgroundColor={colors.JET_BLACK}
             barStyle="light-content"
           />
-          <RootScreen />
+          <PersistGate loading={null} persistor={store.persistor}>
+            <RootScreen />
+          </PersistGate>
         </SafeAreaView>
       </Provider>
     );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,27 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import currentSongReducer from "./reducers/CurrentSongReducer";
-import isSongPlayingReducer from "./reducers/IsSongPlayingReducer";
+import AsyncStorage from '@react-native-community/async-storage';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import thunkMiddleware from 'redux-thunk';
+import currentSongReducer from './reducers/CurrentSongReducer';
+import isSongPlayingReducer from './reducers/IsSongPlayingReducer';
 
 const rootReducer = combineReducers({
   current_song: currentSongReducer,
-  is_song_playing: isSongPlayingReducer
+  is_song_playing: isSongPlayingReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['current_song'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const configureStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunkMiddleware));
+  let store = createStore(persistedReducer, applyMiddleware(thunkMiddleware));
+  let persistor = persistStore(store);
+  return {store, persistor};
 };
 
 export default configureStore;
